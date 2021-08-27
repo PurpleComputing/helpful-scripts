cb7d6feecd6154f5a491b7e3d1cce9aba10f04ea
Michael <30936296+mtan93@users.noreply.github.com>
2021-08-27 09:41:19 +0100

    Update install-rclone-macos.sh

--- install-rclone-macos.sh
+++ install-rclone-macos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Downloading Rclone
-cd && curl -O https://downloads.rclone.org/rclone-current-osx-amd64.zip
+cd /tmp && curl -o rclone-current-osx-amd64.zip https://downloads.rclone.org/rclone-current-osx-amd64.zip
 unzip -a rclone-current-osx-amd64.zip && cd rclone-*-osx-amd64
 sudo mkdir -p /usr/local/bin
 sudo mv rclone /usr/local/bin/
