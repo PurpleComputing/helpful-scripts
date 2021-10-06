8d27042cf988c3131611033034c0c19734904bb6
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-06 17:44:39 +0100

    Update test-raiseticket.sh

--- test-raiseticket.sh
+++ test-raiseticket.sh
@@ -2,16 +2,25 @@ dt=$(date '+%d%m%Y.%H00');
 host=$('hostname');
 user=$('whoami');
 
+echo Command: WindowStyle: Activate >> /var/tmp/depnotify.log
 echo Command: WindowTitle: Create a Support Ticket >> /var/tmp/depnotify.log
-echo Command: Website: https://www.cognitoforms.com/PurpleComputingLimited/SupportRequestForm >> /var/tmp/depnotify.log
-echo Command: ContinueButton: Finished >> /var/tmp/depnotify.log
-echo Command: DeterminateManual: 5 >> /var/tmp/depnotify.log
-echo Command: Command: NotificationOn: >> /var/tmp/depnotify.log
+echo 'Command: Image: /Library/Application Support/Purple/logo.png' >> /var/tmp/depnotify.log
+echo Command: MainText: In a few moments our support ticket form will load, please complete the form and provide as much information as possible. Whilst you are completing the form your Mac will upload diagnostic information to our Help Desk. >> /var/tmp/depnotify.log
+echo Status: Loading form... Thank you. >> /var/tmp/depnotify.log
 
 # START DEPNOTIFY
 curl -o /Users/Shared/.Purple/launch-dep-en.sh https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/launch-dep-en.sh
 chmod +x /Users/Shared/.Purple/launch-dep-en.sh
 screen -dmS DEPFull /Users/Shared/.Purple/launch-dep-en.sh
+
+sleep 12s
+
+echo Command: Website: https://www.cognitoforms.com/PurpleComputingLimited/SupportRequestForm >> /var/tmp/depnotify.log
+echo Command: ContinueButton: Finished >> /var/tmp/depnotify.log
+echo Command: DeterminateManual: 5 >> /var/tmp/depnotify.log
+echo Command: NotificationOn: >> /var/tmp/depnotify.log
+
+
 
 echo Status: Creating System Report, estimated time: 5 minutes >> /var/tmp/depnotify.log
 mkdir -p "/Users/Shared/.Purple/Diagnostics/"
@@ -26,6 +35,7 @@ sleep 3s
 echo Status: Copying Log Files, estimated time: 4 minutes  >> /var/tmp/depnotify.log
 sleep 5s
 cp -r ~/Library/Logs/DiagnosticReports/* "/Users/Shared/.Purple/Diagnostics/$dt/DiagnosticReports"
+defaults read /Library/Preferences/com.teamviewer.teamviewer.preferences.plist ClientID >> "/Users/Shared/.Purple/Diagnostics/$dt/teamviewerID.txt"
 #MORE TO BE ADDED
 
 #echo Command: DeterminateManualStep: 3 >> /var/tmp/depnotify.log
