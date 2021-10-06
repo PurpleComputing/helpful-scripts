3adeca887eeef54fc3f824103cf889fc2efb8609
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-06 17:39:12 +0100

    Update test-raiseticket.sh

--- test-raiseticket.sh
+++ test-raiseticket.sh
@@ -6,7 +6,7 @@ echo Command: WindowTitle: Create a Support Ticket >> /var/tmp/depnotify.log
 echo Command: Website: https://www.cognitoforms.com/PurpleComputingLimited/SupportRequestForm >> /var/tmp/depnotify.log
 echo Command: ContinueButton: Finished >> /var/tmp/depnotify.log
 echo Command: DeterminateManual: 5 >> /var/tmp/depnotify.log
-#echo Command: Command: NotificationOn: >> /var/tmp/depnotify.log
+echo Command: Command: NotificationOn: >> /var/tmp/depnotify.log
 
 # START DEPNOTIFY
 curl -o /Users/Shared/.Purple/launch-dep-en.sh https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/launch-dep-en.sh
@@ -21,24 +21,24 @@ mkdir -p "/Users/Shared/.Purple/Diagnostics/$dt/DiagnosticReports"
 system_profiler >> "/Users/Shared/.Purple/Diagnostics/$dt/system_report.$host.$dt.txt"
 sleep 3s
 
-echo Command: DeterminateManualStep: 2 >> /var/tmp/depnotify.log
+#echo Command: DeterminateManualStep: 2 >> /var/tmp/depnotify.log
 
 echo Status: Copying Log Files, estimated time: 4 minutes  >> /var/tmp/depnotify.log
 sleep 5s
 cp -r ~/Library/Logs/DiagnosticReports/* "/Users/Shared/.Purple/Diagnostics/$dt/DiagnosticReports"
 #MORE TO BE ADDED
 
-echo Command: DeterminateManualStep: 3 >> /var/tmp/depnotify.log
+#echo Command: DeterminateManualStep: 3 >> /var/tmp/depnotify.log
 
 echo Status: Zipping Diagnostics Info, estimated time: 3 minutes  >> /var/tmp/depnotify.log
 sleep 5s
 zip -r "/Users/Shared/.Purple/Diagnostics/"Diagnostics.$user.$host.$dt.zip "/Users/Shared/.Purple/Diagnostics/$dt/"
 
-echo Command: DeterminateManualStep: 4 >> /var/tmp/depnotify.log
+#echo Command: DeterminateManualStep: 4 >> /var/tmp/depnotify.log
 
 echo Status: Sending Diagnotics to Purple Helpdesk, estimated time: 2 minutes  >> /var/tmp/depnotify.log
 sleep 20s
-echo Command: DeterminateManualStep: 5 >> /var/tmp/depnotify.log
+echo Command: DeterminateManualStep: 2 >> /var/tmp/depnotify.log
 echo Status: "Once you have completed the above ticket request please click 'Finished'." >> /var/tmp/depnotify.log
 
 #rm -rf /tmp/brandDEPinstall.sh
