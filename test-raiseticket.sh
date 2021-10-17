b6cf2effd4d8df289e4538680f8b78a16dad3d7a
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-17 13:40:15 +0100

    Update test-raiseticket.sh

--- test-raiseticket.sh
+++ test-raiseticket.sh
@@ -134,7 +134,7 @@ rm -rf "/Users/Shared/.Purple/Diagnostics/$dt"
 
 echo Status: Uploading Diagnotics, estimated time: 2 minutes  >> /var/tmp/depnotify.log
 rm -rf "/Users/Shared/.Purple/Diagnostics/"$dt.uploadurl.txt
-curl --upload-file "/Users/Shared/.Purple/Diagnostics/"Diagnostics.$user.$host.$dt.zip https://purplediagnose-uk.keep.sh -H "Replace" >> "/Users/Shared/.Purple/Diagnostics/"$dt.uploadurl.txt
+curl --upload-file "/Users/Shared/.Purple/Diagnostics/"Diagnostics.$user.$host.$dt.zip https://diagnose.prpl.it -H "Authorization:Basic $authbasic" -H "Replace" -H "Max-Days: 1" >> "/Users/Shared/.Purple/Diagnostics/"$dt.uploadurl.txt
 
 uploadurl=$(cat "/Users/Shared/.Purple/Diagnostics/$dt.uploadurl.txt");
 
