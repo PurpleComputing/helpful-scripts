bcee7cc42aaa510384f033b0826fb42159a10c23
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-17 13:56:10 +0100

    Update test-raiseticket.sh

--- test-raiseticket.sh
+++ test-raiseticket.sh
@@ -153,6 +153,9 @@ echo "Hi Purple Team," >> $SMTPMSG
 echo "" >> $SMTPMSG
 echo "A new file has been uploaded to: $uploadurl containing diagnostic information for $user on $host." >> $SMTPMSG
 echo "" >> $SMTPMSG
+echo "The Username and Password to download are (username:password): $authbasic" >> $SMTPMSG
+echo "The Password to decompress the diagnostics zip is: $$zippass" >> $SMTPMSG
+echo "" >> $SMTPMSG
 echo "Warmest Regards," >> $SMTPMSG
 echo "" >> $SMTPMSG
 echo "Purple MDM Diagnostics Bot" >> $SMTPMSG
