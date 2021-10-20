d313df640c40aee4173814d528536f04a6307d44
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-20 22:17:57 +0100

    Fixed

--- pause-rename.sh
+++ pause-rename.sh
@@ -2,6 +2,6 @@
 dt=$(date '+%d%m%Y.%H%M%S');
 pid=$(ps aux | grep 'rmSBMBv2' | grep -v 'grep' | awk '{print$2}');
 touch /tmp/rename-kill.log
-echo Pause Entry $dt: Killing $pid >> /tmp/rename-kill.log
+echo Pause Entry $dt: Pausing $pid >> /tmp/rename-kill.log
 kill -SIGSTOP $pid  >> /tmp/rename-kill.log
 echo Pause Entry $dt: Finished >> /tmp/rename-kill.log
