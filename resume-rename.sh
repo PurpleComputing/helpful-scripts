db872a9f109455b4167a4f21c31749bb1cd877c6
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-20 21:55:59 +0100

    fix scripts

--- resume-rename.sh
+++ resume-rename.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dt=$(date '+%d%m%Y.%H%M00');
-pid=$(ps aux | grep 'rmSBMBv2' | grep -v 'grep' | awk '{print$2}'
+pid=$(ps aux | grep 'rmSBMBv2' | grep -v 'grep' | awk '{print$2}');
 touch /tmp/rename-kill.log
 echo Resume Entry $dt: Resuming $pid >> /tmp/rename-kill.log
 kill -SIGCONT $pid  >> /tmp/rename-kill.log
