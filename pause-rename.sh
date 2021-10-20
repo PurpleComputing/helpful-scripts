44212d8471c289ad70db818ab6fe4a7534dd23d1
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-20 22:16:45 +0100

    Fixed

--- pause-rename.sh
+++ pause-rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dt=$(date '+%d%m%Y.%H%M00');
+dt=$(date '+%d%m%Y.%H%M%S');
 pid=$(ps aux | grep 'rmSBMBv2' | grep -v 'grep' | awk '{print$2}');
 touch /tmp/rename-kill.log
-echo Kill Entry $dt: Killing $pid >> /tmp/rename-kill.log
+echo Pause Entry $dt: Killing $pid >> /tmp/rename-kill.log
 kill -SIGSTOP $pid  >> /tmp/rename-kill.log
-echo Kill Entry $dt: Finished >> /tmp/rename-kill.log
+echo Pause Entry $dt: Finished >> /tmp/rename-kill.log
--- resume-rename.sh
+++ resume-rename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dt=$(date '+%d%m%Y.%H%M00');
+dt=$(date '+%d%m%Y.%H%M%S');
 pid=$(ps aux | grep 'rmSBMBv2' | grep -v 'grep' | awk '{print$2}');
 touch /tmp/rename-kill.log
 echo Resume Entry $dt: Resuming $pid >> /tmp/rename-kill.log
