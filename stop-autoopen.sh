0f77928510a32b43e20a96f0b3f51c54d878c43e
Michael <30936296+mtan93@users.noreply.github.com>
2021-11-09 19:49:51 +0000

    Update stop-autoopen.sh

--- stop-autoopen.sh
+++ stop-autoopen.sh
@@ -1,7 +1,9 @@
 # RUN SCRIPT ON LOGIN AND LOGOUT TO PREVENT 'Reopen windows when logging back in'.
 # This effectively disables the “reopen windows when logging back in” option,
 # although the checkbox will still appear to be checked. You can just ignore it.
+#
 # Mosyle has the option to add TALLogoutSavesState false and LoginwindowLaunchesRelaunchApps false
 # through it's own config options.
+#
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
