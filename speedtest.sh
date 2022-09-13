b3a0891e1815a3bde063befadd5095d68d4f3322
Michael <30936296+mtan93@users.noreply.github.com>
2022-09-13 18:46:29 +0100

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,4 @@
+cd /Library/Application\ Support/Purple
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+./speedtest-cli --simple 
