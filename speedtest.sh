c3c2d94ebf0d56a850b09e6d6006506e92f75a56
Michael <30936296+mtan93@users.noreply.github.com>
2022-09-13 18:55:20 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,4 @@
 cd /Library/Application\ Support/Purple
-curl -Los speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py --silent
 chmod +x speedtest-cli
 python3 speedtest-cli --simple 
