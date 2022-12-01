589167e34018cdefc89f38dffcf2465ff7525c28
Michael <30936296+mtan93@users.noreply.github.com>
2022-12-01 13:02:04 +0000

    added pentest

--- SecScripts/mt-sec-audit.sh
+++ SecScripts/mt-sec-audit.sh
@@ -21,7 +21,7 @@ brew install git
 git clone https://github.com/CISOfy/lynis
 cd lynis
 ./lynis audit system -Q --auditor "$AUDITORNAME" >> $REPORTOUTPUT
-
+./lynis audit system --pentest --auditor "$AUDITORNAME" >> $REPORTOUTPUT
 chmod 777 /var/log/lynis-report.dat
 chmod 777 $REPORTOUTPUT
 
