8aa387fa1331e6237c0923897e770d25adf4b669
Michael <30936296+mtan93@users.noreply.github.com>
2023-06-15 21:17:02 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -3,5 +3,6 @@ rm -rf speedtest-cli
 curl -s -Lo speedtest-cli https://github.com/PurpleComputing/mdmscripts/raw/main/Helpers/speedtest
 xattr -r -d com.apple.quarantine speedtest-cli
 chmod +x speedtest-cli
-echo
+echo Running Speed Test
+sleep 10
  ./speedtest-cli --accept-license --accept-gdpr >> /tmp/speedtest.log
