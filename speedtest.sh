68195baa14f22d3854b2e2cc03db49648ec52242
Michael <30936296+mtan93@users.noreply.github.com>
2023-06-15 21:12:28 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,7 @@
 cd /Library/Application\ Support/Purple
 rm -rf speedtest-cli
-curl -s -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+curl -s -Lo speedtest-cli https://github.com/PurpleComputing/mdmscripts/raw/main/Helpers/speedtest
+xattr -r -d com.apple.quarantine speedtest-cli
 chmod +x speedtest-cli
 echo
-/usr/local/bin/managed_python3 ./speedtest-cli --simple 
+ ./speedtest-cli --accept-license --accept-gdpr >> /tmp/speedtest.log
