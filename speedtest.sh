31092237aa8b346072b651b3c48d631a3d4ee049
Michael <30936296+mtan93@users.noreply.github.com>
2023-06-15 21:40:36 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,8 +1,18 @@
+runAsUser() {  
+	currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+	currentUserID=$(id -u "$currentUser")
+
+  if [ "$currentUser" != "loginwindow" ]; then
+    /bin/launchctl asuser "$currentUserID" sudo -u "$currentUser" "$@"
+  fi
+}
+
 cd /Library/Application\ Support/Purple
 rm -rf speedtest-cli
 curl -s -Lo speedtest-cli https://github.com/PurpleComputing/mdmscripts/raw/main/Helpers/speedtest
 xattr -r -d com.apple.quarantine speedtest-cli
-chmod +x speedtest-cli
+chmod 777 speedtest-cli
 echo Running Speed Test
 sleep 10
- ./speedtest-cli --accept-license --accept-gdpr >> /tmp/speedtest.log
+
+runAsUser /Library/Application\ Support/Purple/speedtest-cli --accept-license --accept-gdpr >> /tmp/speedtest.log
