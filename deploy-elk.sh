681faf2ee3e59048e1d1394f63ee1468fe79e361
Michael <30936296+mtan93@users.noreply.github.com>
2023-06-30 14:14:17 +0100

    Update deploy-elk.sh

--- deploy-elk.sh
+++ deploy-elk.sh
@@ -15,7 +15,18 @@
 ###############################################################################################
 # NOTICE: UBUNTU-SPECIFIC SCRIPT, WE RECOMMEND 22.04 LTS
 ###############################################################################################
-
+ELKPATH=/opt/es/
 hostname elastic-stack
 
-mkdir -p /opt/es/
+mkdir -p $ELKPATH
+
+cd $ELKPATH
+
+git clone https://github.com/elkninja/elastic-stack-docker-part-one.git
+
+mv elastic-stack-docker-part-one elastic-stack
+cd elastic-stack
+
+curl -s https://raw.githubusercontent.com/PurpleComputing/helpful-scripts/main/install-docker-ubuntu.sh | bash
+
+docker-compose up -d
