e94097ba9ac004cf04346a96aa3a6dbac5a713f5
Michael <30936296+mtan93@users.noreply.github.com>
2023-07-01 10:42:03 +0100

    Update deploy-elk.sh

--- deploy-elk.sh
+++ deploy-elk.sh
@@ -22,11 +22,9 @@ mkdir -p $ELKPATH
 
 cd $ELKPATH
 
-git clone https://github.com/elkninja/elastic-stack-docker-part-one.git
+git clone https://github.com/spujadas/elk-docker.git
 
-mv elastic-stack-docker-part-one elastic-stack
-cd elastic-stack
 
-curl -s https://raw.githubusercontent.com/PurpleComputing/helpful-scripts/main/install-docker-ubuntu.sh | bash
+#curl -s https://raw.githubusercontent.com/PurpleComputing/helpful-scripts/main/install-docker-ubuntu.sh | bash
 
 #docker-compose up -d
