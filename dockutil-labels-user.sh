e3511cd9e80191ee1282a0c9fe6b22057279fcd0
Michael <30936296+mtan93@users.noreply.github.com>
2023-07-07 18:20:37 +0100

    Update dockutil-labels-user.sh

--- dockutil-labels-user.sh
+++ dockutil-labels-user.sh
@@ -27,213 +27,287 @@ fi
 if [[ " $@ " =~ "safari" ]]; then
 	APPNA="Safari"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "googlechrome" ]]; then
 	APPNA="Google Chrome"
 	APPPA="/Applications"
-	
+
+fi
+if [[ " $@ " =~ "googlechromeenterprise" ]]; then
+	APPNA="Google Chrome"
+	APPPA="/Applications"
+
 fi
 if [[ " $@ " =~ "firefox" ]]; then
 	APPNA="Firefox"
 	APPPA="/Applications"
-	
+
+fi
+if [[ " $@ " =~ "brave" ]]; then
+	APPNA="Brave Browser"
+	APPPA="/Applications"
+
 fi
 
 # MICROSOFT OFFICE APPS
 if [[ " $@ " =~ "microsoftword" ]]; then
 	APPNA="Microsoft Word"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "microsoftexcel" ]]; then
 	APPNA="Microsoft Excel"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "microsoftpowerpoint" ]]; then
 	APPNA="Microsoft Powerpoint"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "microsoftoutlook" ]]; then
 	APPNA="Microsoft Outlook"
 	APPPA="/Applications"
-	
-fi
 
+fi
+if [[ " $@ " =~ "microsoftonenote" ]]; then
+	APPNA="Microsoft OneNote"
+	APPPA="/Applications"
+
+fi
 
 # CLOUD STORAGE APPS
 if [[ " $@ " =~ "dropbox" ]]; then
 	APPNA="Dropbox"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "microsoftonedrive" ]]; then
 	APPNA="OneDrive"
 	APPPA="/Applications"
-	
+
 fi
 
 # VIDEO MEETING APPS
 if [[ " $@ " =~ "slack" ]]; then
 	APPNA="Slack"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "microsoftteams" ]]; then
 	APPNA="Microsoft Teams"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "zoom" ]]; then
 	APPNA="zoom.us"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "skype" ]]; then
 	APPNA="Skype"
 	APPPA="/Applications"
-	
+
 fi
 # ADOBE APPS
 if [[ " $@ " =~ "photoshop2022" ]]; then
 	APPNA="Adobe Photoshop 2022"
 	APPPA="/Applications/Adobe Photoshop 2022"
-	
+
 fi
 if [[ " $@ " =~ "indesign2022" ]]; then
 	APPNA="Adobe InDesign 2022"
 	APPPA="/Applications/Adobe InDesign 2022"
-	
+
 fi
 if [[ " $@ " =~ "illustrator2022" ]]; then
 	APPNA="Adobe Illustrator"
 	APPPA="/Applications/Adobe Illustrator 2022"
-	
+
 fi
 if [[ " $@ " =~ "acrobatdc" ]]; then
 	APPNA="Adobe Acrobat"
 	APPPA="/Applications/Adobe Acrobat DC"
-	
+
 fi
 if [[ " $@ " =~ "adobereaderdc" ]]; then
-    APPNA="Adobe Acrobat Reader"
-    APPPA="/Applications"
-    
+	APPNA="Adobe Acrobat Reader"
+	APPPA="/Applications"
+
 fi
 if [[ " $@ " =~ "adobecreativeclouddesktop" ]]; then
 	APPNA="Creative Cloud"
 	APPPA="/Applications/Utilities/Adobe Creative Cloud/ACC"
-	
+
 fi
 #OTHERS
+if [[ " $@ " =~ "console" ]]; then
+	APPNA="Console"
+	APPPA="/Applications/Utilities"
+
+fi
+if [[ " $@ " =~ "activitymonitor" ]]; then
+	APPNA="Activity Monitor"
+	APPPA="/Applications/Utilities"
+
+fi
+if [[ " $@ " =~ "diskutility" ]]; then
+	APPNA="Disk Utility"
+	APPPA="/Applications/Utilities"
+
+fi
+if [[ " $@ " =~ "terminal" ]]; then
+	APPNA="Terminal"
+	APPPA="/Applications/Utilities"
+
+fi
 if [[ " $@ " =~ "mail" ]]; then
 	APPNA="Mail"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "maps" ]]; then
 	APPNA="Maps"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "news" ]]; then
 	APPNA="News"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "filezilla" ]]; then
 	APPNA="FileZilla"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "teamviewer" ]]; then
 	APPNA="TeamViewer"
 	APPPA="/Applications"
-	
+
+fi
+if [[ " $@ " =~ "teamviewerhost" ]]; then
+	APPNA="TeamViewer Host"
+	APPPA="/Applications"
+
+fi
+if [[ " $@ " =~ "appleremotedesktop" ]]; then
+	APPNA="Remote Desktop"
+	APPPA="/Applications"
+
+fi
+if [[ " $@ " =~ "screensharing" ]]; then
+	APPNA="Screen Sharing"
+	APPPA="/System/Library/CoreServices/Applications"
+
 fi
 if [[ " $@ " =~ "microsoftremotedesktop" ]]; then
 	APPNA="Microsoft Remote Desktop"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "1password8" ]]; then
 	APPNA="1Password"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "1password7" ]]; then
 	APPNA="1Password 7"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "filemakerpro" ]]; then
 	APPNA="FileMaker Pro"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "keka" ]]; then
 	APPNA="Keka"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "icons" ]]; then
 	APPNA="Icons"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "cyberduck" ]]; then
 	APPNA="Cyberduck"
 	APPPA="/Applications"
-	
+
+fi
+if [[ " $@ " =~ "asana" ]]; then
+	APPNA="Asana"
+	APPPA="/Applications"
+
+fi
+if [[ " $@ " =~ "googledrive" ]]; then
+	APPNA="Google Drive"
+	APPPA="/Applications"
+
 fi
 # NOT KNOWN TO INSTALLOMATOR - USE FOR DOCK POPULATION ONLY
 
 if [[ " $@ " =~ "zywallsecuextender" ]]; then
 	APPNA="ZyWALL SecuExtender"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "redstorproese" ]]; then
 	APPNA="Redstor Pro ESE"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "mitelconnect" ]]; then
 	APPNA="Mitel Connect"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "transmit" ]]; then
 	APPNA="Transmit"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "monday" ]]; then
 	APPNA="monday.com"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "filemaker15" ]]; then
 	APPNA="FileMaker Pro"
 	APPPA="/Applications/FileMaker Pro 15"
-	
+
 fi
 if [[ " $@ " =~ "excellcloudvoice" ]]; then
 	APPNA="Excell Cloud Voice Plus"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "accountedgene" ]]; then
 	APPNA="AccountEdge NE"
 	APPPA="/Applications"
-	
+
 fi
 if [[ " $@ " =~ "3cx" ]]; then
 	APPNA="3CX Desktop App"
 	APPPA="/Applications"
-	
+
+fi
+if [[ " $@ " =~ "nordlayer" ]]; then
+	APPNA="NordLayer"
+	APPPA="/Applications"
+
+fi
+if [[ " $@ " =~ "nordpass" ]]; then
+	APPNA="NordPass"
+	APPPA="/Applications"
+
+fi
+if [[ " $@ " =~ "btcloudphone" ]]; then
+	APPNA="BT Cloud Phone"
+	APPPA="/Applications"
+
 fi
 # ********************************************************************************************************************************
 # END APP LABELS
@@ -244,21 +318,28 @@ fi
 # MOSYLE SELF SERVICE
 if [[ " $@ " =~ "mosyless" ]]; then
 	APPNA="Self-Service"
-	APPPA="/Applications"    
+	APPPA="/Applications"
 	DOCKPOS=2
 fi
 
 # SYSTEM PREFERENCES
 if [[ " $@ " =~ "systempreferences" ]]; then
 	APPNA="System Preferences"
-	APPPA="/Applications"    
-	
+	APPPA="/Applications"
+
+fi
+
+# SYSTEM PREFERENCES
+if [[ " $@ " =~ "systemsettings" ]]; then
+	APPNA="System Settings"
+	APPPA="/Applications"
+
 fi
 
 # LAUNCHPAD
 if [[ " $@ " =~ "launchpad" ]]; then
 	APPNA="Launchpad"
-	APPPA="/Applications"    
+	APPPA="/Applications"
 	DOCKPOS=1
 fi
 
