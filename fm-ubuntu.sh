9b0129654dc4d07d1d0bfb537becae8f83e22780
Michael <30936296+mtan93@users.noreply.github.com>
2025-06-11 12:22:06 +0100

    Create fm-ubuntu.sh

---
+++ fm-ubuntu.sh
@@ -0,0 +1,31 @@
+# Update Ubuntu, run this multiple times until there are no more updates
+apt update && apt upgrade
+
+# Check & confirm Ubunto version
+lsb_release -a
+
+# Install needed packages
+apt install wget
+apt install unzip
+apt install at
+
+# Install FileMaker Server
+cd ~/
+mkdir fminstaller
+cd fminstaller
+
+# Download and unzip
+wget https://downloads.claris.com/esd/fms_21.1.5.500_Ubuntu22_amd64.zip
+unzip fms_21.1.5.500_Ubuntu22_amd64.zip
+
+# Identify installation filename
+ls filemaker*.deb
+
+# Start installation
+sudo apt install ./fms_*.deb
+
+# Follow prompts during installation for license, type and admin credentials
+
+# Check & confirm FileMaker Server & Nginx are running
+ps -A | grep fm
+ps -A | grep nginx
